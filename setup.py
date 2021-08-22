50abec1f0f7df9200d8931694a9bb5fb136c9ffc
Piotr Domanski <piotrjerzydomanski@gmail.com>
2021-08-22 20:12:28 +0200

    Update of .gitignore README and setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
 
-setup(name='Smart Security',
-      version='1.0',
-      description='Module to avoid writing many similar decorators and attaching them to many methods',
+setup(
+      name='Smart Security',
+      version='2.0',
+      description=(
+            "Extension of django-guardian to allow "
+            "check permissions recursively based on models"
+      ),
       author='Piotr Domanski',
       author_email='piotrjerzydomanski@gmail.com',
       packages=['smart_security'],
-      )
+      install_requires=["django-guardian>=2.0.0"]
+)

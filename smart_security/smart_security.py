9c763c1ef685be77072c4c6d4a38569c50f87d11
Piotr Domanski <piotrjerzydomanski@gmail.com>
2021-08-29 13:58:43 +0200

    Handled case of unique permissions

--- smart_security/smart_security.py
+++ smart_security/smart_security.py
@@ -1,11 +1,12 @@
 from logging import getLogger
-from typing import Optional, Union, Type, List
+from typing import Optional, Union, Type, List, Tuple
 
 from django.apps import apps
 from django.conf import settings
 from django.contrib.auth.models import User, Permission
 from django.db.models import Model
 from guardian.backends import ObjectPermissionBackend
+from guardian.ctypes import get_content_type
 
 from smart_security.constants import (
     SMART_SECURITY_MODEL_CLASS_SETTING,
@@ -23,20 +24,45 @@ class SmartSecurityObjectPermissionBackend(ObjectPermissionBackend):
     def has_perm(
         self, user_obj: User, perm: Union[str, Permission], obj: Optional[Model] = None
     ) -> bool:
+        perm = self._get_permission_codename(perm)
         if obj is not None:
-            security_model_class = self._get_security_model_class()
-            model_class = obj.__class__
-            if security_model_class != model_class:
-                shortest = self._find_shortest_accessor(
-                    model_class, security_model_class
-                )
-                for accessor in shortest:
-                    obj = getattr(obj, accessor)
-                perm = perm.replace(
-                    self._get_permission_name_from_model_name(model_class),
-                    self._get_permission_name_from_model_name(security_model_class),
-                )
+            obj, perm = self._get_obj_and_perm(obj, perm)
         return super().has_perm(user_obj, perm, obj=obj)
+
+    def _get_obj_and_perm(self, obj: Model, perm: str) -> Tuple[Model, str]:
+        security_model_class = self._get_security_model_class()
+        model_class = obj.__class__
+        if security_model_class != model_class:
+            owner = self._get_owner(model_class, obj, security_model_class)
+            owner_perm = perm.replace(
+                self._get_permission_name_from_model_name(model_class),
+                self._get_permission_name_from_model_name(security_model_class),
+            )
+            if self._should_apply_smart_security(owner, owner_perm):
+                obj = owner
+                perm = owner_perm
+        return obj, perm
+
+    def _get_owner(
+        self, model_class: Type[Model], obj: Model, security_model_class: Type[Model]
+    ) -> Model:
+        shortest = self._find_shortest_accessor(model_class, security_model_class)
+        for accessor in shortest:
+            obj = getattr(obj, accessor)
+        return obj
+
+    @classmethod
+    def _get_permission_codename(cls, perm: Union[str, Permission]) -> str:
+        if isinstance(perm, Permission):
+            return perm.codename
+        return perm
+
+    @classmethod
+    def _should_apply_smart_security(cls, owner: Model, owner_perm: str) -> bool:
+        content_type = get_content_type(owner)
+        return Permission.objects.filter(
+            codename=owner_perm, content_type=content_type
+        ).exists()
 
     @classmethod
     def _find_shortest_accessor(
--- tests/test_project/test_app/models.py
+++ tests/test_project/test_app/models.py
@@ -10,8 +10,19 @@ from django.db.models import (
 class TestOwner(Model):
     name = TextField(primary_key=True)
 
+    class Meta:
+        permissions = [
+            ("not_unique_permission", "Not unique permission"),
+        ]
+
 
 class TestBroker(Model):
+    class Meta:
+        permissions = [
+            ("unique_permission", "Unique permission"),
+            ("not_unique_permission", "Not unique permission"),
+        ]
+
     owner = ForeignKey(TestOwner, on_delete=CASCADE)
 
 
